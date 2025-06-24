50fc16b8ed55abc11944bf884123ea3e63166aea
Bryan McClanahan <bmcclanahan@amctheatres.com>
2025-06-24 15:09:19 -0500

    remove payload logo and HeaderNav

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,13 +1,10 @@
 'use client'
 import { useHeaderTheme } from '@/providers/HeaderTheme'
-import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 import type { Header } from '@/payload-types'
 
-import { Logo } from '@/components/Logo/Logo'
-import { HeaderNav } from './Nav'
 
 interface HeaderClientProps {
   data: Header
@@ -32,10 +29,10 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   return (
     <header className="container relative z-20   " {...(theme ? { 'data-theme': theme } : {})}>
       <div className="py-8 flex justify-between">
-        <Link href="/">
+        {/* <Link href="/">
           <Logo loading="eager" priority="high" className="invert dark:invert-0" />
         </Link>
-        <HeaderNav data={data} />
+        <HeaderNav data={data} /> */}
       </div>
     </header>
   )
